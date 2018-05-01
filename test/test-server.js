4a2912cf70d846b007ae96ba9adac9e3fc1dec15
Joshua Zuo <neveragain8209@yahoo.com>
2018-05-01 07:46:49 -0500

    added tests and travis

---
+++ test/test-server.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../index');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Blog', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  it('should list blogs on Get', function() {
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      expect(ers).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      const expectedKeys = ['content', 'id', 'author', 'publishDate', 'title'];
+      res.body.forEach(function(item) {
+        expect(item).to.be.a('object')
+        expect(item).to.include.keys(expectedKeys);
+      });
+    });
+  });
+});
